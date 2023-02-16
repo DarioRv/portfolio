166d1387b531f13e37e5c79f99f9bb3b65f465d0
DarioRv <dariorvidal.27@gmail.com>
2023-02-16 19:04:24 -0300

    Agregado de peticiones put, del y get al servicio de portfolio.

--- src/app/services/portfolio-data.service.ts
+++ src/app/services/portfolio-data.service.ts
@@ -19,17 +19,66 @@ export class PortfolioDataService {
   addNewLaboralExperience(experience: any): Observable<any> {
     return this.http.post(`${this.API}/new/laboral-experience`, experience);
   }
+  editLaboralExperience(experience: any): Observable<any> {
+    return this.http.put(`${this.API}/update/laboral-experience`, experience);
+  }
+  deleteLaboralExperience(experienceId: any): Observable<any> {
+    return this.http.delete(`${this.API}/delete/laboral-experience?id=${experienceId}`);
+  }
+  getLaboralExperience(): Observable<any> {
+    return this.http.get(`${this.API}/view/laboral-experience`);
+  }
 
   addNewEducation(education: any): Observable<any> {
     return this.http.post(`${this.API}/new/education`, education);
+  }
+  editEducation(education: any): Observable<any> {
+    return this.http.put(`${this.API}/update/education`, education);
+  }
+  deleteEducation(educationId: any): Observable<any> {
+    return this.http.delete(`${this.API}/delete/education?id=${educationId}`);
+  }
+  getEducation(): Observable<any> {
+    return this.http.get(`${this.API}/view/education`);
   }
 
   addNewSkill(skill: any): Observable<any> {
     return this.http.post(`${this.API}/new/skill`, skill);
   }
+  editSkill(skill: any): Observable<any> {
+    return this.http.put(`${this.API}/update/skill`, skill);
+  }
+  deleteSkill(skillId: any): Observable<any> {
+    return this.http.delete(`${this.API}/delete/skill?id=${skillId}`);
+  }
+  getSkills(): Observable<any> {
+    return this.http.get(`${this.API}/view/skills`);
+  }
 
   addNewProject(project: any): Observable<any> {
     return this.http.post(`${this.API}/new/project`, project);
+  }
+  editProject(project: any): Observable<any> {
+    return this.http.put(`${this.API}/update/project`, project);
+  }
+  deleteProject(projectId: any): Observable<any> {
+    return this.http.delete(`${this.API}/delete/project?id=${projectId}`);
+  }
+  getProjects(): Observable<any> {
+    return this.http.get(`${this.API}/view/projects`);
+  }
+
+  addNewContact(contact: any): Observable<any> {
+    return this.http.post(`${this.API}/new/contact`, contact)
+  }
+  editContact(contact: any): Observable<any> {
+    return this.http.put(`${this.API}/new/contact`, contact)
+  }
+  deleteContact(contactId: any): Observable<any> {
+    return this.http.delete(`${this.API}/new/contact?id=${contactId}`)
+  }
+  getContact(): Observable<any> {
+    return this.http.get(`${this.API}/view/contacts`)
   }
 
 }
